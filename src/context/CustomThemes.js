ba198cde85a1db0b8efc66f6e58cc02b428e4e21
Satyen Sen <Satyendra.sant7@gmail.com>
2023-01-08 00:51:09 +0530

    Custom Theme Mui - Template

---
+++ src/context/CustomThemes.js
@@ -0,0 +1,24 @@
+export const customThemes = {
+    dark: {
+        divider: "#4A61CE",
+        background: {
+            default: "#11193F",
+            paper: "#26388D",
+        },
+        text: {
+            primary: "#F6F6F7",
+            secondary: "#747A99",
+        },
+    },
+    light: {
+        divider: "#747A99",
+        background: {
+            default: "#FFFFFF",
+            paper: "#F6F6F7",
+        },
+        text: {
+            primary: "#26388D",
+            secondary: "#4A61CE",
+        },
+    },
+};
--- src/context/index.js
+++ src/context/index.js
@@ -1,35 +1,11 @@
 import React, { createContext, useMemo, useState } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { customThemes } from './CustomThemes';
 
 export const ColorModeContext = createContext({
     toggleColorMode: () => { },
     mode: 'light',
 })
-
-const themesObject = {
-    dark: {
-        divider: "#4A61CE",
-        background: {
-            default: "#11193F",
-            paper: "#26388D",
-        },
-        text: {
-            primary: "#F6F6F7",
-            secondary: "#747A99",
-        },
-    },
-    light: {
-        divider: "#747A99",
-        background: {
-            default: "#FFFFFF",
-            paper: "#F6F6F7",
-        },
-        text: {
-            primary: "#26388D",
-            secondary: "#4A61CE",
-        },
-    },
-}
 
 export const ColorContextProvider = ({ children }) => {
     const [mode, setMode] = useState('light');
@@ -42,7 +18,7 @@ export const ColorContextProvider = ({ children }) => {
     const theme = useMemo(() => createTheme({
         palette: {
             mode: mode,
-            ...themesObject[mode]
+            ...customThemes[mode]
         },
     }), [mode])
 
